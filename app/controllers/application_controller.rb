2d6dc40907418b26356d8ff642d22fdd8ed4206e
Lukas Mills <lukascodes64@gmail.com>
2022-08-22 07:47:40 -0400

    commit tree 0173fe8ab366fdfbc74a8af80a3466b04a41b5ab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do 
+    
+    bakeries = Bakery.all
+
+    bakeries.to_json
+  end
 
 end
